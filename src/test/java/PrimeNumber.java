fd4ecd61ca340318342f1c01b25b27bf10c3f4ac
pravin687 <pravintambare1@gmail.com>
2022-05-11 08:30:21 +0530

    programs

---
+++ src/test/java/PrimeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		System.out.println("please enter number");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+
+		if (number <= 1) {
+			System.out.println("Not a Prime No");
+		}
+
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				System.out.println("Not a Prime No");
+			}
+
+		}
+		System.out.println("Its a Prime No");
+	}
+}

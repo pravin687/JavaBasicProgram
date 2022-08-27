8f91e5a7bde2015a55e2adc6c936caed7200457d
pravin687 <pravintambare1@gmail.com>
2022-08-27 11:40:10 +0530

    Akshay 2nd commit

--- src/test/java/basicprogram/Gitest.java
+++ src/test/java/basicprogram/Gitest.java
@@ -6,6 +6,8 @@ public class Gitest {
 		// TODO Auto-generated method stub
 
 		System.out.println("My 1st commit");
+		System.out.println("My 2nd commit");
+		System.out.println("My 3rd commit");
 	}
 
 }

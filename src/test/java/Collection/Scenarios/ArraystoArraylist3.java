e469c6aefc274c8b46da43210661699511be341d
Pravin <pravintambare1@gmail.com>
2023-03-20 13:48:33 +0530

    Test

---
+++ src/test/java/Collection/Scenarios/ArraystoArraylist3.java
@@ -0,0 +1,26 @@
+package Collection.Scenarios;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraystoArraylist3 {
+	
+	public static void main(String[] args) {
+		String[] a={"Pravin","Anjali","Amish","Manisha"};
+		//Array to list
+		List l=Arrays.asList(a);
+		Collections.sort(l);
+		//for(String b:a)
+		//System.out.println(b);
+		//convert list to array
+		Object[] c=l.toArray();
+		for(int i=0;i<c.length;i++) {
+			System.out.println(c[i]);
+		}
+		
+		
+	}
+
+	
+}
